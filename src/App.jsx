fab2a4846b57c9d0b05d5c7c6ee24e73c61d669b
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-08-08 20:37:07 -0300

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 
+//components
+import NavBar from './components/NavBar';
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
+      <NavBar/>
      <div className="container">
         <Outlet/>
      </div>
