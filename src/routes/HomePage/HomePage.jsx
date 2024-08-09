dd9795b6f53f828b8db0f1174ff2d985aa05ab97
Dev Matheus Peluso <matheuspeluso17@gmail.com>
2024-08-09 00:02:58 -0300

    get data from the api

--- src/routes/HomePage/HomePage.jsx
+++ src/routes/HomePage/HomePage.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+//css
+import "./HomePage.css";
 
 const HomePage = () => {
+
+  //resgatando os dados dos posts da api json placeholder
+  const [post,setPost] = useState([]);
+
+  const getPost = async() => {
+    try {
+      
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const data = response.data;
+      console.log(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getPost()
+  },[]) // array vazio - executa apenas 1 vez
+
   return (
     <div>
       HomePage
